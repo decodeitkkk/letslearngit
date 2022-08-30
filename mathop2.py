25e2759d0275c72b2483f4d121468ec6a27671ef
decodeitkkk <kkailash0086@gmail.com>
2022-08-30 22:07:03 +0530

    subtraction added

--- mathop2.py
+++ mathop2.py
@@ -1,11 +1,21 @@
 a = 0
 while a != 1:
     print("""1. Addition\n2. Subtraction\n3. Multiplication\n4. Division""")
-    if op == "Add":
+    op = int(input("Enter your choice:-"))
+    if op == 1:
         try:
             n1 = int(input("Enter Num 1:-"))
             n2 = int(input("Enter Num 2:-"))
             sum = n1 + n2
+            print(f"This is your sum:-{sum}")
+            a += 1
+        except Exception as e:
+            print(e)
+    elif op == 2:
+        try:
+            n1 = int(input("Enter Num 1:-"))
+            n2 = int(input("Enter Num 2:-"))
+            sum = n1 - n2
             print(f"This is your sum:-{sum}")
             a += 1
         except Exception as e:
