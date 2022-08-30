e7322c1a6c544d156b7b1c3f3180d906aa11c8c2
decodeitkkk <kkailash0086@gmail.com>
2022-08-30 21:05:04 +0530

    except error added

--- mathoperations.py
+++ mathoperations.py
@@ -1,4 +1,11 @@
-n1 = int(input("Enter Num 1:-"))
-n2 = int(input("Enter Num 2:-"))
-sum = n1 + n2
-print(f"This is your sum:-")
+
+a = 0
+while a != 1:
+    try:
+        n1 = int(input("Enter Num 1:-"))
+        n2 = int(input("Enter Num 2:-"))
+        sum = n1 + n2
+        print(f"This is your sum:-{sum}")
+        a += 1
+    except Exception as e:
+        print(e)
