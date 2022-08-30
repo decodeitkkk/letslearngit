41d41ac912705a9fc2cb3f71c47b8acfef8327d5
decodeitkkk <kkailash0086@gmail.com>
2022-08-30 23:34:13 +0530

    error removed

--- mathop2.py
+++ mathop2.py
@@ -1,13 +1,13 @@
 a = 0
 while a != 1:
     print("""1. Addition\n2. Subtraction\n3. Multiplication\n4. Division""")
-    if op == "Add":
-        try:
-            n1 = int(input("Enter Num 1:-"))
-            n2 = int(input("Enter Num 2:-"))
-            sum = n1 + n2
-            print(f"This is your sum:-{sum}")
-            a += 1
-        except Exception as e:
-            print(e)
+
+    try:
+        n1 = int(input("Enter Num 1:-"))
+        n2 = int(input("Enter Num 2:-"))
+        sum = n1 + n2
+        print(f"This is your sum:-{sum}")
+        a += 1
+    except Exception as e:
+        print(e)
 
